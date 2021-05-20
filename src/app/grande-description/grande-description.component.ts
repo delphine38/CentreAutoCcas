a601bef920f0cffb8a9a9fa81abf7c6b8faf99ad
delphine <delphin38@live.fr>
2021-05-20 20:33:49 +0200

    ajout de photo arriere et tableau de bord dans Grande description

--- src/app/grande-description/grande-description.component.ts
+++ src/app/grande-description/grande-description.component.ts
@@ -8,7 +8,7 @@ import { Voiture } from '../model/voiture.model';
 })
 export class GrandeDescriptionComponent implements OnInit {
 
-  voiture = new Voiture("nenenen","ejejejje","Très belle Audi A3, peu rouler, confort cuir de vachette, couleur camel","30 000", "2020", "120 000", "Audi", "A1", "Electrique","AudiA1");
+  voiture = new Voiture("nenenen","Audi A1","Très belle Audi A3, peu rouler, confort cuir de vachette, couleur camel. Electrique /110 000km. Boite automatique pour confort de conduite optimisé. Palette au volant, tableau de bord intuitif. Bluetooth. Possibilité de discution de prix à l'essai.","30 000", "2020", "120 000", "Audi", "A1", "Electrique","AudiA1");
 
 // descriptionLongue: Voiture[]=[
 //   {
